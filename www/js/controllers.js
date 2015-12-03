31a93f129d6c916cf1f82fc9eb0d7b93b65f7188
AugustinChini <augustin.chini@hotmail.fr>
2015-12-03 16:32:18 +0100

    fix bug get query, add cordova-plugin-whitelist

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,52 +5,11 @@ appZZcloud.controller('getWebDavContent', function($scope, $http) {
                 'Authorization': 'Basic YXVndXN0aW46c2F1Y2lzc2U8Mw=='
             }
         };
-        $http.get("http://achini.ddns.net/owncloud/remote.php/webdav/", config)
-        .success(function(data) {
+        $http.get("http://achini.ddns.net/owncloud/remote.php/webdav/", config).then(function(data) {
             $scope.arbo = data;
-        })
-        .error(function(data) {
-            alert("ERROR");
+        }, function(data) {
+            $scope.arbo = data;
         });
-        /*var url = 'http://achini.ddns.net/owncloud/remote.php/webdav/';
-        var username = "augustin";
-        var password = "saucisse<3";
-
-        var xhr = $scope.createCORSRequest('GET', url, username, password);
-        if (!xhr) {
-            $scope.arbo = "Erreur : requête CORS non supporté.";
-            return;
-        }
-
-        // écouteurs le réponse async.
-        xhr.onload = function() {
-            $scope.arbo = xhr.responseText;
-        };
-
-        xhr.onerror = function() {
-            $scope.arbo = "Erreur lors de la création de la requête"
-        };
-        
-        xhr.send();*/
     }
-
-        // création de l'objet XHR.
-    /*$scope.createCORSRequest = function createCORSRequest(method, url, username, password) {
-        var xhr = new XMLHttpRequest();
-        if ("withCredentials" in xhr) {
-            // XHR pour Chrome/Firefox/Opera/Safari.
-            xhr.open(method, url, true, username, password);
-            xhr.withCredentials = true;
-        } else if (typeof XDomainRequest != "undefined") {
-            // IE.
-            xhr = new XDomainRequest();
-            xhr.open(method, url, true, username, password);
-            xhr.withCredentials = true;
-        } else {
-            // CORS non supporté.
-            xhr = null;
-        }
-        return xhr;
-    }*/
 
 });
